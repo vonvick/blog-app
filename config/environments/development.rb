140b5d3efcf8d4b8525429fb6e4c57954f4a646e
Victor Nwaiwu <victor.nwaiwu@andela.com>
2018-10-05 00:46:34 +0100

    feature: remove confirmation fields from user model amd migration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,10 +31,24 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # config.action_mailer.default_url_options = { host: ENV['HOST'], port: ENV['PORT'] }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
+
+  config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: ENV['HOST'], port: ENV['PORT'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
+  config.action_mailer.smtp_settings = {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'music-library-app.herokuapp.com',
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- db/schema.rb
+++ db/schema.rb
@@ -43,14 +43,9 @@ ActiveRecord::Schema.define(version: 20181004203623) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.json "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["role_id"], name: "index_users_on_role_id"
