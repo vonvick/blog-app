24959a89d36bb2d62e46691418d1181521257cca
Victor Nwaiwu <victor.nwaiwu@andela.com>
2018-10-11 23:43:46 +0100

    feature: create concerns for handling file uploads

---
+++ app/controllers/concerns/uploads.rb
@@ -0,0 +1,25 @@
+module Concerns
+  module Uploads
+    extend ActiveSupport::Concern
+
+    def upload_file
+      load_resource.upload_resource
+    end
+
+    def delete_file
+      load_resource.delete_resource
+    end
+
+    def load_resource
+      CloudinaryServices.new(item_options)
+    end
+
+    def item_options
+      {
+        url: url,
+        upload_type: resource_type,
+        resource_options: resource_option
+      }
+    end
+  end
+end
