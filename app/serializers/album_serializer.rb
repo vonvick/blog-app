b04da120bd9ae2895fb2950d3391498fc246844b
Victor Nwaiwu <victor.nwaiwu@andela.com>
2018-10-12 00:21:06 +0100

    feature: update serializers to display image and song resources

--- app/serializers/album_serializer.rb
+++ app/serializers/album_serializer.rb
@@ -1,5 +1,5 @@
 class AlbumSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :artist, :year, :created_at, :updated_at
+  attributes :id, :title, :description, :artist, :year, :image_url, :created_at, :updated_at
 
   belongs_to :created_by, class_name: 'User'
   has_many :ratings
